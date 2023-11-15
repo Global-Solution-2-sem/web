b7de5eda923cb7f88ef2ab5013e129b03d8fe740
FelipeHeilmann <felipeheilmannm@gmail.com>
2023-11-15 20:44:13 -0300

    feat: aside about application component

---
+++ frontend/src/components/Application.tsx
@@ -0,0 +1,20 @@
+export default function Application() {
+    return (
+        <aside>
+            <div>
+                <h2>O que é a solução</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius corporis ipsum quasi tempore ab voluptates, vitae, voluptatibus odit est impedit incidunt architecto harum numquam. Ullam amet molestias consequuntur dolore consectetur.</p>
+            </div>
+
+            <div>
+                <h2>O que ela fará</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius corporis ipsum quasi tempore ab voluptates, vitae, voluptatibus odit est impedit incidunt architecto harum numquam. Ullam amet molestias consequuntur dolore consectetur.</p>
+            </div>
+
+            <div>
+                <h2>Como funcionará</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius corporis ipsum quasi tempore ab voluptates, vitae, voluptatibus odit est impedit incidunt architecto harum numquam. Ullam amet molestias consequuntur dolore consectetur.</p>
+            </div>
+        </aside>
+    )
+}
