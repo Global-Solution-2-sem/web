654064ec71d75f84c54b722013ac9789a6939e39
FelipeHeilmann <felipeheilmannm@gmail.com>
2023-11-15 20:04:31 -0300

    feat: header

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,10 @@
+import '../styles/header.scss'
+
+export default function Header() {
+    return (
+        <header>
+            <h1>Title</h1>
+            <p>Global Solution 2023 - Engenharia de Software</p>
+        </header>
+    )
+}
