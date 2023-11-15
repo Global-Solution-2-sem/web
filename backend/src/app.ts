4ed0f20a3110901cd1aa1c64d915d0f8a7af3130
FelipeHeilmann <felipeheilmannm@gmail.com>
2023-11-14 21:27:50 -0300

    feat: api and login route

---
+++ backend/src/app.ts
@@ -0,0 +1,53 @@
+import fastify from 'fastify'
+import jwt from '@fastify/jwt'
+
+const app = fastify()
+
+app.register(jwt, {
+    secret: 'AULA-DE-JS'
+})
+
+const users = [
+    {
+        id: "f06ca828-966c-47bd-bcae-d8adb47cef5d",
+        name: "admin",
+        email: "admin@admin",
+        password: "admin" 
+    }
+]
+
+
+app.post('/login', async function (req,reply) {
+    const {email, password} = req.body as {email: string, password: string}
+
+    const userExists = users.find(user => user.email === email)
+
+    if(userExists === undefined){
+        reply.status(400).send({message: 'Usuário não encontrado'})
+    }
+
+    if(userExists?.password !== password){
+        reply.status(400).send({message: 'Usuário ou senha inválidos'})
+    }
+
+    const token = app.jwt.sign(
+        {
+            name: userExists!.name,
+            email: userExists!.email,
+        },
+        {
+            sub: userExists!.id,
+            expiresIn: '1 days',
+        }
+    )
+
+    reply.status(200).send({token})
+
+})
+
+app.listen({
+    port: 5000,
+    host: '0.0.0.0'
+}).then(() => {
+    console.log(`HTTP server running on port ${5000}`)
+})
