24becd85fc7de85ce2497398010756ae28ca9f95
FelipeHeilmann <felipeheilmannm@gmail.com>
2023-11-16 11:00:32 -0300

    feat: login function and error handle

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,22 +1,41 @@
-import '../styles/login.scss'
+import axios from 'axios';
+import '../styles/login.scss';
+import { useForm } from 'react-hook-form';
+import { useState } from 'react'
 
 export default function Login() {
+    const { register, handleSubmit } = useForm()
+    const [errorMessage, setErrorMessage] = useState<string>()
+
+    /*eslint-disable */
+    const onSubmit = async (data: any) => {
+        setErrorMessage('')
+        try {
+            const response = await axios.post('http://localhost:5000/auth', data)
+
+            localStorage.setItem('token', response.data.token)
+        }
+        catch (err: any) {
+            setErrorMessage(err.response.data.message)
+        }
+    }
     return (
         <main>
             <section>
-                <form action="">
+                <form action="" onSubmit={handleSubmit(onSubmit)}>
                     <div className="input-item">
-                        <label htmlFor="">Usuário</label>
-                        <input type="text" />
+                        <label htmlFor="user">Usuário</label>
+                        <input type="text" id='user' {...register("email")} />
                     </div>
 
                     <div className="input-item">
-                        <label htmlFor="">Senha</label>
-                        <input type="text" />
+                        <label htmlFor="password">Senha</label>
+                        <input type="password" id='password'  {...register("password")} />
                     </div>
                     <div className="input-item">
                         <button type="submit">Login</button>
                     </div>
+                    {errorMessage && <p className='error-message'>{errorMessage}</p>}
                 </form>
             </section>
         </main>
