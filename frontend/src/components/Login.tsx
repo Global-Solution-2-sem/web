8b1a9a3c736e205d44d423d45af086c0088d6f38
FelipeHeilmann <felipeheilmannm@gmail.com>
2023-11-16 10:38:11 -0300

    feat: login component

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,5 +1,24 @@
+import '../styles/login.scss'
+
 export default function Login() {
     return (
-        <h1>login</h1>
+        <main>
+            <section>
+                <form action="">
+                    <div className="input-item">
+                        <label htmlFor="">Usuário</label>
+                        <input type="text" />
+                    </div>
+
+                    <div className="input-item">
+                        <label htmlFor="">Senha</label>
+                        <input type="text" />
+                    </div>
+                    <div className="input-item">
+                        <button type="submit">Login</button>
+                    </div>
+                </form>
+            </section>
+        </main>
     )
 }
