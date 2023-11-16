cf7ed815dbf3541f0ac75ae17fd7465d14405bfc
FelipeHeilmann <felipeheilmannm@gmail.com>
2023-11-15 21:10:14 -0300

    feat: images component style

--- frontend/src/components/Images.tsx
+++ frontend/src/components/Images.tsx
@@ -1,7 +1,17 @@
 export default function Images() {
     return (
         <section>
-            <div className="main-image"></div>
+            <figure>
+                <div className="main-image"></div>
+            </figure>
+            <div>
+                <h2>
+                    Nome da aplicação criada
+                </h2>
+                <div className="text-container">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum nisi autem atque voluptate quo aliquid, odit laborum, harum earum eligendi debitis maiores! Cupiditate laboriosam, consequatur magnam omnis qui provident facere.</p>
+                </div>
+            </div>
         </section>
     )
 }
